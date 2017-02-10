d41d9405ba7e3efaff2a3df7b9d2e3e7fb502d25
jietpriyanka <priyanka.rani@jietjodhpur.ac.in>
2017-02-10 09:54:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/ui/page 1', function(req,res)
-  res.send("this is page 1 response");
+  res.send("this is page 1 response")
     
     );
 
